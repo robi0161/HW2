0df0ab68d52a97f2dbce1638efeafe5ad5f6d6f6
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-09-17 23:40:27 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -8,7 +8,7 @@ echo getDisplay();
 include "view-footer.php";
 
 function getDisplay() {
-  if (isset($_POST['my-name'])) {
+if (isset($_POST['my-name'])) {
    return "<p>The value sent is:</p>". $_POST['my-name'];
 } else {
   return "<p>Nothing posted to the page.</p>";
